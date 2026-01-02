eb3bc34e4fe82b23ca81d88deaea29eb9041f025
Thiago Santos <thiagodlsantos9@gmail.com>
2026-01-01 22:21:50 -0300

    change chsarp icon

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,15 +1,23 @@
 return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      "nvim-tree/nvim-web-devicons", 
-    },
-    lazy = false, 
-    config = function()
-      vim.keymap.set('n', '<C-n>', ':Neotree filesystem toggle left<CR>', {})
-    end
-  }
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			"nvim-tree/nvim-web-devicons",
+		},
+		lazy = false,
+		config = function()
+			require("nvim-web-devicons").set_icon({
+				csharp = {
+					icon = "",
+					color = "#89e051",
+					cterm_color = "58",
+					name = "cs",
+				},
+			})
+			vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle left<CR>", {})
+		end,
+	},
 }
