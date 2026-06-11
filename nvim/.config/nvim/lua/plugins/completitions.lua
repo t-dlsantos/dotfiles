b10f309c76186a8b813d1707cde4bbcb49e5dfa1
Thiago de Lima Santos <thiagodlsantos9@gmail.com>
2026-06-11 18:00:15 -0300

    a bunch of new things

--- nvim/.config/nvim/lua/plugins/completitions.lua
+++ nvim/.config/nvim/lua/plugins/completitions.lua
@@ -1,62 +1,33 @@
 return {
-	{
-		"hrsh7th/cmp-nvim-lsp",
+	"saghen/blink.cmp",
+	dependencies = {
+		"saghen/blink.lib",
+		"rafamadriz/friendly-snippets",
 	},
-	{
-		"L3MON4D3/LuaSnip",
-		dependencies = {
-			"saadparwaiz1/cmp_luasnip",
-			"rafamadriz/friendly-snippets",
+
+	opts = {
+		keymap = { preset = "super-tab" },
+
+		completion = {
+			menu = {
+				auto_show = true,
+        auto_show_delay_ms = 100
+			},
+			trigger = {
+				show_on_keyword = true,
+				show_on_trigger_character = true,
+			},
+			documentation = {
+				auto_show = true,
+			},
 		},
-	},
-	{
-		"hrsh7th/nvim-cmp",
-		config = function()
-      local luasnip = require("luasnip")
-			local cmp = require("cmp")
-			require("luasnip.loaders.from_vscode").lazy_load()
 
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
-					end,
-				},
-				window = {
-					completion = cmp.config.window.bordered(),
-					documentation = cmp.config.window.bordered(),
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
-					["<Tab>"] = cmp.mapping(function(fallback)
-						if luasnip.locally_jumpable(1) then
-							luasnip.jump(1)
-						else
-							fallback()
-						end
-					end, { "i", "s" }),
+		sources = { default = { "lsp", "path", "snippets", "buffer" } },
 
-					["<S-Tab>"] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_prev_item()
-						elseif luasnip.locally_jumpable(-1) then
-							luasnip.jump(-1)
-						else
-							fallback()
-						end
-					end, { "i", "s" }),
-				}),
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" },
-				}, {
-					{ name = "buffer" },
-				}),
-			})
-		end,
+		signature = {
+			enabled = true,
+		},
+
+		fuzzy = { implementation = "lua" },
 	},
 }
--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -2,10 +2,10 @@ return {
 	{
 		"mason-org/mason.nvim",
 		opts = {
-      ensure_installed = {
-        "netcoredbg"
-      }
-    },
+			ensure_installed = {
+				"netcoredbg",
+			},
+		},
 		config = function()
 			require("mason").setup({
 				registries = {
@@ -26,12 +26,24 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local capabilities = require("blink.cmp").get_lsp_capabilities()
+
+			vim.lsp.config("*", {
+				capabilities = capabilities,
+			})
+
+			vim.lsp.enable({
+				"lua_ls",
+				"jsonls",
+				"ruff",
+				"pyright",
+				"ts_ls",
+			})
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-      vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "LSP references" })
+			vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "LSP references" })
 		end,
 	},
 }
